27a2f9a2aae46768d6952296770d5512994e287e
Mihaiela Nemes <mihaiela.nemes@stellenanzeigen.de>
2020-10-19 17:02:52 +0200

    unicafe.step1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Button = ({text, onclick}) => {
+    return (
+        <button onClick={onclick}>{text}</button>
+    )
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Statistic = (props) => {
+    return (
+        <div> {props.text} {props.count}</div>
+    )
+};
+
+const App = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const increaseCount = (value, setMethod) => {
+        return setMethod(value + 1);
+    };
+
+   return (
+       <>
+            <h1> Give Feedback </h1>
+            <Button text='good' onclick={() => increaseCount(good, setGood)}/>
+            <Button text='neutral' onclick={() => increaseCount(neutral, setNeutral)} />
+            <Button text='bad' onclick={() => increaseCount(bad, setBad)}/>
+
+           <h1>Statistic</h1>
+           <Statistic text='good' count={good}/>
+           <Statistic text='neutral' count={neutral}/>
+           <Statistic text='bad' count={bad}/>
+
+       </>
+   )
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+

edbbf6668ec436bc526c9b7470c7f83ebda2f011
Mihaiela Nemes <mihaiela.nemes@stellenanzeigen.de>
2020-10-24 10:52:04 +0200

    Part2 - unicafe.Step7_8

--- src/Part2/Content.js
+++ src/Part2/Content.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Part from "./Part";
+import Total from "./Total";
 
 
 const Content = ({parts}) => {
+
+    const sum = parts.reduce( (sum, p) => sum + p.exercises, 0);
+
     return (
         <div>
-            { parts.map( (part) =>
+            {
+                parts.map( (part) =>
                 <Part name={part.name} exercises={part.exercises}/>
-            )}
+                )
+            }
+            {
+                parts.reduce( (sum, p) => sum+p.exercises)
+            }
+            <Total total={sum} />
         </div>
     )
 };
---
+++ src/Part2/Total.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Total = ({total}) => {
+    return (
+        <p>Total of {total} exercises</p>
+    )
+};
+
+export default Total
